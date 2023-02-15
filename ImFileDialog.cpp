8aeb53ea8ed624d9a06fc32c6e183e0fc8ababac
Jimmy Lord <jimmylord@gmail.com>
2023-02-15 11:40:14 -0500

    Changed IsKeyPressed param to the typed enum required by imgui 1.89+.

--- ImFileDialog.cpp
+++ ImFileDialog.cpp
@@ -1408,7 +1408,7 @@ namespace ifd {
 
 		int escapeKey = ImGui::GetIO().KeyMap[ImGuiKey_Escape];
 		if (ImGui::IsWindowFocused(ImGuiFocusedFlags_RootAndChildWindows) &&
-			 escapeKey >= 0 && ImGui::IsKeyPressed(escapeKey))
+			 escapeKey >= 0 && ImGui::IsKeyPressed(ImGuiKey_Escape))
 			m_isOpen = false;
 	}
 }
